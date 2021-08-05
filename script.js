59f0303c3d8cf0eee937fbcb230f7b0303c0f3a7
Metis <zeenrejent1234@hotmail.com>
2021-08-05 14:54:54 +0700

    quiz

--- script.js
+++ script.js
@@ -4,10 +4,24 @@ const btn_toggle = document.querySelector("#toggle");
 const length = document.getElementById('length')
 const color = document.querySelector('#color')
 
+
 // define more constants and variables here
 
 btn_toggle.onclick = () => {
   // your code here
+  document.getElementById("toggle").click();
 }
-
+function search(){
+  var str = "";
+  var n=str.length.search(value)
+  n.setAttribute("type","color");
+  document.body.appendChild(n);
+ 
+}
+function reset(){
+  var str="";
+  var n=str.length.reset(value)
+  n.setAttribute("type","reset");
+  document.body.appendChild(n);
+}
 // more codes for Search and Reset buttons here
